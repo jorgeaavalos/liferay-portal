2a2b8180fdf6013bd63a8b66c01d5b1d5632d66d
Jorge Avalos <jorge.avalos@liferay.com>
2026-07-23 13:40:10 -0700

    LPD-92619 SF

--- modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommand.java
+++ modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommand.java
@@ -74,10 +74,10 @@ public class ExportInstanceMVCActionCommand extends BaseMVCActionCommand {
 					GetterUtil.getString(exception.getMessage())));
 		}
 
-		hideDefaultSuccessMessage(actionRequest);
-
 		JSONPortletResponseUtil.writeJSON(
 			actionRequest, actionResponse, jsonObject);
+
+		hideDefaultSuccessMessage(actionRequest);
 	}
 
 	private void _checkPermission() throws PrincipalException {
