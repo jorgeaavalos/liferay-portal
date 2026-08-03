6b793e7cd95fa9e99e5de19053cb4917e199c595
Jorge Avalos <jorge.avalos@liferay.com>
2026-08-03 13:13:55 -0700

    LPD-92619 Cover the scoped configuration export
    (cherry picked from commit 3cc9c08ad28c50d199d0635e3b42984ce0482d79)

--- modules/apps/portal-instances/portal-instances-web/src/test/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommandTest.java
+++ modules/apps/portal-instances/portal-instances-web/src/test/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommandTest.java
@@ -5,6 +5,9 @@
 
 package com.liferay.portal.instances.web.internal.portlet.action;
 
+import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 import com.liferay.portal.db.partition.util.DBPartitionUtil;
 import com.liferay.portal.json.JSONObjectImpl;
 import com.liferay.portal.kernel.exception.RequiredCompanyException;
@@ -12,17 +15,22 @@ import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.portlet.MockActionRequest;
 import com.liferay.portal.kernel.test.portlet.MockActionResponse;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
@@ -30,7 +38,10 @@ import jakarta.portlet.ActionRequest;
 import jakarta.portlet.ActionResponse;
 import jakarta.portlet.PortletRequest;
 
+import java.util.Collections;
 import java.util.Locale;
+
+import org.apache.felix.cm.file.ConfigurationHandler;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -74,9 +85,21 @@ public class ExportInstanceMVCActionCommandTest {
 			_exportInstanceMVCActionCommand, "_companyService",
 			_companyService);
 		ReflectionTestUtil.setFieldValue(
+			_exportInstanceMVCActionCommand, "_groupLocalService",
+			_groupLocalService);
+		ReflectionTestUtil.setFieldValue(
 			_exportInstanceMVCActionCommand, "_jsonFactory", _jsonFactory);
 		ReflectionTestUtil.setFieldValue(
 			_exportInstanceMVCActionCommand, "_language", _language);
+
+		_dbPartitionUtilMockedStatic = Mockito.mockStatic(
+			DBPartitionUtil.class);
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			Collections.emptyMap()
+		);
 
 		Mockito.when(
 			_permissionChecker.isOmniadmin()
@@ -113,8 +136,207 @@ public class ExportInstanceMVCActionCommandTest {
 	public void tearDown() {
 		PermissionThreadLocal.setPermissionChecker(null);
 
+		_dbPartitionUtilMockedStatic.close();
 		_featureFlagManagerUtilMockedStatic.close();
 		_jsonPortletResponseUtilMockedStatic.close();
+	}
+
+	@Test
+	public void testConfigurationExportedForCompanyScope() throws Exception {
+		long companyId = RandomTestUtil.randomLong();
+		String configurationId = RandomTestUtil.randomString();
+
+		String encodedDictionary = _getEncodedDictionary(
+			ExtendedObjectClassDefinition.Scope.COMPANY, companyId);
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			HashMapBuilder.put(
+				configurationId, encodedDictionary
+			).build()
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				companyId, configurationId, encodedDictionary));
+	}
+
+	@Test
+	public void testConfigurationExportedForGroupScope() throws Exception {
+		long companyId = RandomTestUtil.randomLong();
+
+		long groupId = RandomTestUtil.randomLong();
+
+		String configurationId = RandomTestUtil.randomString();
+		String encodedDictionary = _getEncodedDictionary(
+			ExtendedObjectClassDefinition.Scope.GROUP, groupId);
+
+		_setUpGroup(groupId, companyId);
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			HashMapBuilder.put(
+				configurationId, encodedDictionary
+			).build()
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				companyId, configurationId, encodedDictionary));
+	}
+
+	@Test
+	public void testConfigurationNotExportedForMissingGroup() throws Exception {
+		long companyId = RandomTestUtil.randomLong();
+
+		long groupId = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			_groupLocalService.fetchGroup(groupId)
+		).thenReturn(
+			null
+		);
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			HashMapBuilder.put(
+				RandomTestUtil.randomString(),
+				_getEncodedDictionary(
+					ExtendedObjectClassDefinition.Scope.GROUP, groupId)
+			).build()
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString()),
+			Mockito.never());
+	}
+
+	@Test
+	public void testConfigurationNotExportedForNullEncodedDictionary()
+		throws Exception {
+
+		long companyId = RandomTestUtil.randomLong();
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			Collections.<String, String>singletonMap(
+				RandomTestUtil.randomString(), null)
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString()),
+			Mockito.never());
+	}
+
+	@Test
+	public void testConfigurationNotExportedForOtherCompanyGroupScope()
+		throws Exception {
+
+		long companyId = RandomTestUtil.randomLong();
+
+		long groupId = RandomTestUtil.randomLong();
+
+		_setUpGroup(groupId, RandomTestUtil.randomLong());
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			HashMapBuilder.put(
+				RandomTestUtil.randomString(),
+				_getEncodedDictionary(
+					ExtendedObjectClassDefinition.Scope.GROUP, groupId)
+			).build()
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString()),
+			Mockito.never());
+	}
+
+	@Test
+	public void testConfigurationNotExportedForOtherCompanyScope()
+		throws Exception {
+
+		long companyId = RandomTestUtil.randomLong();
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			HashMapBuilder.put(
+				RandomTestUtil.randomString(),
+				_getEncodedDictionary(
+					ExtendedObjectClassDefinition.Scope.COMPANY,
+					RandomTestUtil.randomLong())
+			).build()
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString()),
+			Mockito.never());
+	}
+
+	@Test
+	public void testErrorMessageForConfigurationExportFailure()
+		throws Exception {
+
+		long companyId = RandomTestUtil.randomLong();
+
+		String message = RandomTestUtil.randomString();
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenThrow(
+			new IllegalStateException(message)
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(companyId), new MockActionResponse());
+
+		_assertErrorMessage("export-failed-with-message-x:" + message);
+
+		Mockito.verify(
+			_companyService
+		).exportCompany(
+			companyId
+		);
 	}
 
 	@Test
@@ -203,6 +425,10 @@ public class ExportInstanceMVCActionCommandTest {
 		_exportInstanceMVCActionCommand.doProcessAction(
 			_getMockActionRequest(companyId), new MockActionResponse());
 
+		_assertErrorMessage(expectedErrorMessage);
+	}
+
+	private void _assertErrorMessage(String expectedErrorMessage) {
 		Assert.assertEquals(
 			expectedErrorMessage, _jsonObject.getString("error"));
 		Assert.assertFalse(_jsonObject.has("successMessage"));
@@ -211,6 +437,23 @@ public class ExportInstanceMVCActionCommandTest {
 			() -> JSONPortletResponseUtil.writeJSON(
 				Mockito.any(ActionRequest.class),
 				Mockito.any(ActionResponse.class), Mockito.eq(_jsonObject)));
+	}
+
+	private String _getEncodedDictionary(
+			ExtendedObjectClassDefinition.Scope scope, Object scopePK)
+		throws Exception {
+
+		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+			new UnsyncByteArrayOutputStream();
+
+		ConfigurationHandler.write(
+			unsyncByteArrayOutputStream,
+			HashMapDictionaryBuilder.<String, Object>put(
+				scope.getPropertyKey(), scopePK
+			).build());
+
+		return new String(
+			unsyncByteArrayOutputStream.toByteArray(), StringPool.UTF8);
 	}
 
 	private MockActionRequest _getMockActionRequest(long companyId) {
@@ -223,8 +466,25 @@ public class ExportInstanceMVCActionCommandTest {
 		return mockActionRequest;
 	}
 
+	private void _setUpGroup(long groupId, long companyId) {
+		Group group = Mockito.mock(Group.class);
+
+		Mockito.when(
+			group.getCompanyId()
+		).thenReturn(
+			companyId
+		);
+
+		Mockito.when(
+			_groupLocalService.fetchGroup(groupId)
+		).thenReturn(
+			group
+		);
+	}
+
 	private final CompanyService _companyService = Mockito.mock(
 		CompanyService.class);
+	private MockedStatic<DBPartitionUtil> _dbPartitionUtilMockedStatic;
 
 	private final ExportInstanceMVCActionCommand
 		_exportInstanceMVCActionCommand = new ExportInstanceMVCActionCommand() {
@@ -238,6 +498,8 @@ public class ExportInstanceMVCActionCommandTest {
 
 	private MockedStatic<FeatureFlagManagerUtil>
 		_featureFlagManagerUtilMockedStatic;
+	private final GroupLocalService _groupLocalService = Mockito.mock(
+		GroupLocalService.class);
 	private final JSONFactory _jsonFactory = Mockito.mock(JSONFactory.class);
 	private JSONObject _jsonObject;
 	private MockedStatic<JSONPortletResponseUtil>
