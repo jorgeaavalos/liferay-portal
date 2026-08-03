1a982bf2640e14085d6eb7874b402f52ea7daf38
Jorge Avalos <jorge.avalos@liferay.com>
2026-08-03 12:55:03 -0700

    LPD-92619 Export scoped configurations with the instance
    The headless export endpoint copies the instance's scoped OSGi configurations into the exported schema, but the portlet action did not, so an export triggered from the Virtual Instances UI produced a schema missing them. The step is a no-op when database partitioning is enabled, because the exported partition already carries the configurations, and only does work when it is disabled. The logic mirrors PortalInstanceResourceImpl so both entry points export the same content.
    
    (cherry picked from commit 2d8728a0303ceca6369a62666753adc41bc69b1b)

--- modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommand.java
+++ modules/apps/portal-instances/portal-instances-web/src/main/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommand.java
@@ -5,6 +5,10 @@
 
 package com.liferay.portal.instances.web.internal.portlet.action;
 
+import com.liferay.petra.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 import com.liferay.portal.db.partition.util.DBPartitionUtil;
 import com.liferay.portal.instances.web.internal.constants.PortalInstancesPortletKeys;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
@@ -13,17 +17,30 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.portlet.ActionRequest;
 import jakarta.portlet.ActionResponse;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.felix.cm.file.ConfigurationHandler;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -61,6 +78,8 @@ public class ExportInstanceMVCActionCommand extends BaseMVCActionCommand {
 		try {
 			_companyService.exportCompany(companyId);
 
+			_exportConfigurations(companyId);
+
 			jsonObject.put(
 				"successMessage",
 				_language.format(
@@ -89,6 +108,135 @@ public class ExportInstanceMVCActionCommand extends BaseMVCActionCommand {
 		hideDefaultSuccessMessage(actionRequest);
 	}
 
+	private void _exportConfigurations(long companyId) throws Exception {
+		if (PropsValues.DATABASE_PARTITION_ENABLED) {
+			return;
+		}
+
+		List<ScopedConfiguration> scopedConfigurations = new ArrayList<>();
+
+		Map<String, String> configurations = DBPartitionUtil.getConfigurations(
+			CompanyConstants.SYSTEM);
+
+		for (Map.Entry<String, String> entry : configurations.entrySet()) {
+			ScopedConfiguration scopedConfiguration = _getScopedConfiguration(
+				entry.getKey(), entry.getValue());
+
+			if (scopedConfiguration == null) {
+				continue;
+			}
+
+			if (Objects.equals(
+					scopedConfiguration.getScope(),
+					ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE)) {
+
+				scopedConfigurations.add(scopedConfiguration);
+
+				continue;
+			}
+
+			if (_isApplicable(companyId, scopedConfiguration)) {
+				scopedConfigurations.add(scopedConfiguration);
+			}
+		}
+
+		for (ScopedConfiguration scopedConfiguration : scopedConfigurations) {
+			DBPartitionUtil.exportConfiguration(
+				companyId, scopedConfiguration.getConfigurationId(),
+				scopedConfiguration.getEncodedDictionary());
+		}
+	}
+
+	private ScopedConfiguration _getScopedConfiguration(
+			String configurationId, String encodedDictionary)
+		throws Exception {
+
+		if (Validator.isNull(encodedDictionary)) {
+			return null;
+		}
+
+		Dictionary<String, String> dictionary = ConfigurationHandler.read(
+			new UnsyncByteArrayInputStream(
+				encodedDictionary.getBytes(StringPool.UTF8)));
+
+		Object value = dictionary.get(
+			ExtendedObjectClassDefinition.Scope.GROUP.getPropertyKey());
+
+		if (value != null) {
+			return new ScopedConfiguration(
+				configurationId, encodedDictionary,
+				ExtendedObjectClassDefinition.Scope.GROUP,
+				GetterUtil.getLong(value));
+		}
+
+		value = dictionary.get(
+			ExtendedObjectClassDefinition.Scope.COMPANY.getPropertyKey());
+
+		if (value != null) {
+			return new ScopedConfiguration(
+				configurationId, encodedDictionary,
+				ExtendedObjectClassDefinition.Scope.COMPANY,
+				GetterUtil.getLong(value));
+		}
+
+		value = dictionary.get(
+			ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE.
+				getPropertyKey());
+
+		if (value != null) {
+			return new ScopedConfiguration(
+				configurationId, encodedDictionary,
+				ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE,
+				GetterUtil.getString(value));
+		}
+
+		return null;
+	}
+
+	private boolean _isApplicable(
+		long companyId, ScopedConfiguration scopedConfiguration) {
+
+		if (Objects.equals(
+				scopedConfiguration.getScope(),
+				ExtendedObjectClassDefinition.Scope.COMPANY)) {
+
+			if (companyId == (long)scopedConfiguration.getScopePK()) {
+				return true;
+			}
+
+			return false;
+		}
+
+		if (Objects.equals(
+				scopedConfiguration.getScope(),
+				ExtendedObjectClassDefinition.Scope.GROUP)) {
+
+			long groupId = (long)scopedConfiguration.getScopePK();
+
+			Group group = _groupLocalService.fetchGroup(groupId);
+
+			if (group == null) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						StringBundler.concat(
+							"Unable to export configuration ",
+							scopedConfiguration.getConfigurationId(),
+							" because group ", groupId, " does not exist"));
+				}
+
+				return false;
+			}
+
+			if (group.getCompanyId() == companyId) {
+				return true;
+			}
+
+			return false;
+		}
+
+		return true;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ExportInstanceMVCActionCommand.class);
 
@@ -96,9 +244,47 @@ public class ExportInstanceMVCActionCommand extends BaseMVCActionCommand {
 	private CompanyService _companyService;
 
 	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
 	private JSONFactory _jsonFactory;
 
 	@Reference
 	private Language _language;
+
+	private static class ScopedConfiguration {
+
+		public ScopedConfiguration(
+			String configurationId, String encodedDictionary,
+			ExtendedObjectClassDefinition.Scope scope, Object scopePK) {
+
+			_configurationId = configurationId;
+			_encodedDictionary = encodedDictionary;
+			_scope = scope;
+			_scopePK = scopePK;
+		}
+
+		public String getConfigurationId() {
+			return _configurationId;
+		}
+
+		public String getEncodedDictionary() {
+			return _encodedDictionary;
+		}
+
+		public ExtendedObjectClassDefinition.Scope getScope() {
+			return _scope;
+		}
+
+		public Object getScopePK() {
+			return _scopePK;
+		}
+
+		private final String _configurationId;
+		private final String _encodedDictionary;
+		private final ExtendedObjectClassDefinition.Scope _scope;
+		private final Object _scopePK;
+
+	}
 
 }
